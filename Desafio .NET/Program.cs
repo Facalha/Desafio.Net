03a1b447730ea62961f9e4b534b02f4dec3787a7
Vítor Facalha <102127307+Facalha@users.noreply.github.com>
2023-07-14 23:29:48 -0300

    Teste01

--- Desafio .NET/Program.cs
+++ Desafio .NET/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Digite a nota do primeiro semestre:");
+float notaPrimeiroSemestre = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a nota do segundo semestre:");
+float notaSegundoSemestre = Convert.ToInt32(Console.ReadLine());
+
+float media = (notaPrimeiroSemestre + notaSegundoSemestre) / 2;
+
+Console.WriteLine($"Sua média é de {media}");
