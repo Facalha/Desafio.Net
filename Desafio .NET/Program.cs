71eaa806d54c8728358a2d84ac6b8a4ef1547af1
Vítor Facalha <102127307+Facalha@users.noreply.github.com>
2023-07-14 23:31:07 -0300

    Teste02

--- Desafio .NET/Program.cs
+++ Desafio .NET/Program.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 
-Console.WriteLine("Digite a nota do primeiro semestre:");
-float notaPrimeiroSemestre = Convert.ToInt32(Console.ReadLine());
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Nome do Usuário
+        Console.WriteLine("Bem-vindo ao Teste 02, para iniciarmos qual é o seu nome?");
+        string nomeDoUsuario = Console.ReadLine();
 
-Console.WriteLine("Digite a nota do segundo semestre:");
-float notaSegundoSemestre = Convert.ToInt32(Console.ReadLine());
+        // Verificando se a string não está vazia
+        if (string.IsNullOrEmpty(nomeDoUsuario))
+        {
+            Console.WriteLine("Nome inválido. Por favor, digite novamente.");
+            return; // Encerra o programa se o nome for inválido
+        }
 
-float media = (notaPrimeiroSemestre + notaSegundoSemestre) / 2;
+        int numeroDeNotas = 0;
+        double totalDasNotas = 0;
 
-Console.WriteLine($"Sua média é de {media}");
+        for (int quantidadeDeProvas = 1; ; quantidadeDeProvas++)
+        {
+            // Pega e armazena nota
+            double notaDaProva = PegaNota();
+
+            totalDasNotas += notaDaProva;
+            numeroDeNotas++;
+
+            double media = totalDasNotas / numeroDeNotas;
+
+            Console.WriteLine($"Nome: {nomeDoUsuario}");
+            Console.WriteLine($"Quantidade de Provas: {quantidadeDeProvas}");
+            Console.WriteLine($"Total de Nota: {totalDasNotas}");
+            Console.WriteLine($"Sua média é de {media}");
+        }
+
+        double PegaNota()
+        {
+            Console.WriteLine($"{nomeDoUsuario}, por favor, nos informe qual foi sua nota na prova?");
+            double notaDaProva = double.Parse(Console.ReadLine());
+
+            return notaDaProva;
+        }
+    }
+}
